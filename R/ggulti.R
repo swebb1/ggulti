b3f3850be02b7d4ba3359342a135e0d010c2916d
swebb1 <shaun.webb@ed.ac.uk>
2024-05-09 13:02:58 +0100

    Added roxygen docs

--- R/ggulti.R
+++ R/ggulti.R
@@ -1,4 +1,3 @@
-library(ggsoccer)
 library(ggplot2)
 library(dplyr)
 library(purrr)
@@ -7,7 +6,27 @@ library(tidyr)
 library(gganimate)
 library(gifski)
 
-ggpitch <- function (colour = "dimgray", fill = "white", type = "full", endzone_fill = "blue",
+#' Generate a pitch map as a ggplot object. The 'type' parameter controls the section of pitch.
+#' Default size is set to 100 x 37, with endzones 18 deep.
+#'
+#' @param type Style of pitch: "full" (default), "half_attack", "half_defend", "blank", "void"
+#' @param colour Colour of pitch outline : default = "dimgrey"
+#' @param fill Fill colour of central zone  : default = "white"
+#' @param endzone_fill Fill colour of endzone : default = #AA4422
+#' @param linewidth Width of lines : default = 0.5
+#' @param alpha Fill opacity of central zone : default = 1
+#' @param endzone_alpha Fill opacity of endzone : default = 0.2
+#' @param linetype Linetype of lines : default = solid
+#' @param xmin Start x coordinate : default = 0
+#' @param xmax End x coordinate : default = 37
+#' @param ymin Start y coordinate : default = 0
+#' @param ymax End y coordinate : default = 100
+#' @param zone_depth Depth of endzones : default = 18
+#' @return A ggplot object
+#' @examples
+#' ggpitch()
+#' ggpitch("half_attack",endzone_fill="blue")
+ggpitch <- function (type = "full", colour = "dimgray", fill = "white", endzone_fill = "#AA4422",
                      linewidth = 0.5, alpha = 1, endzone_alpha=0.2, linetype = "solid",
                      xmin = 0, xmax = 37, ymin = 0, ymax = 100, zone_depth = 18)
 {
@@ -40,6 +59,11 @@ ggpitch <- function (colour = "dimgray", fill = "white", type = "full", endzone_
   p
 }
 
+#' Helper function to print a list of 7 players in horizontal stack formation
+#'
+#' @return A tibble
+#' @examples
+#' hstack
 hstack <- function(){
   players <- list(
     player("H1", show=T, x=c(8), y=c(22), alpha = 0.8, frame = 1),
@@ -53,7 +77,23 @@ hstack <- function(){
   bind_rows(players)
 }
 
-player <- function(label = "", show = F, object = "Offense", x, y, frame = 1, alpha = 0.5, label_pos = "down"){
+#' Create a pitch_object dataframe. Objects are represented with geom_point in the plot_play() function.
+#' Objects are usually players on the field.
+#'
+#' @param label Name for the object (required)
+#' @param show Show the label in plots : default = F
+#' @param object Name for the type of object : "Offense" (default), "Defense", "Coach", "Cone", "Disc" are recognised but can be anything.
+#' @param x Vector of x-coords covered by the object (required)
+#' @param y Vector of y-coords covered by the object (required)
+#' @param frame Vector of frames to show the object in : default = 1
+#' @param alpha Colour opacity of the object : default = 0.5
+#' @param label_pos Position of label relative to object : "down" (default), "up", "left", "right"
+#' @return A data.frame
+#' @examples
+#' pitch_object("H1", T, x = c(15,15,5), y = c(25,35,37), frame = 1:3)
+#' pitch_object("D1", F, object = "Defense", x = c(13,13,8), y = c(25,35,37), frame = 1:3)
+#' pitch_object("q1", F, object = "Cone", x = c(13), y = c(26), frame = 1:3)
+pitch_object <- function(label = "", show = F, object = "Offense", x, y, frame = 1, alpha = 0.5, label_pos = "down"){
   lshow = ""
   if(show == T){
     lshow = label
@@ -61,7 +101,21 @@ player <- function(label = "", show = F, object = "Offense", x, y, frame = 1, al
   data.frame(label = label, show = lshow, object = object, x = x, y = y, frame = frame, alpha = alpha, label_pos = label_pos)
 }
 
-defender <-function(player,label = "", show = NA, object = "Defense", xjust, yjust, frame = NA, alpha = NA, label_pos = NA){
+#' Convenience function to create a Defense pitch_object dataframe which tracks another objects movements.
+#'
+#' @param player Name of the object_dataframe to track (required)
+#' @param label Name for the object (required)
+#' @param show Show the label in plots : default = Same as player object, T, F
+#' @param object Name for the type of object : "Defense" (default), "Offense", "Coach", "Cone", "Disc" are recognised but can be anything.
+#' @param xjust Vector of adjustments to player x-coords to offset defenders position : default = -1
+#' @param yjust Vector of adjustments to player y-coords to offset defenders position : default = -1
+#' @param frame Vector of frames to show the object in : default = same as player object
+#' @param alpha Colour opacity of the object : default = same as player object
+#' @param label_pos Position of label relative to object : default = same as player object, "down"  "up", "left", "right"
+#' @return A data.frame
+#' @examples
+#' defender(player_list[[1]], "D1", x = c(-1,-1,5), y = c(2,2,5))
+defender <-function(player,label = "", show = NA, object = "Defense", xjust = -1, yjust = -1, frame = NA, alpha = NA, label_pos = NA){
   if(is.na(frame)){
     frame = player$frame
   }
@@ -81,6 +135,25 @@ defender <-function(player,label = "", show = NA, object = "Defense", xjust, yju
   data.frame(label = label, show = lshow, object = object, x = player$x + xjust, y = player$y + yjust, frame = frame, alpha = alpha, label_pos = label_pos)
 }
 
+#' Create a pitch_arrow dataframe. Arrows are represented with geom_segment or geom_curve in the plot_play() function.
+#' Arrows are usually player movements (cuts) or throws on the field.
+#'
+#' @param label Name for the arrow (required)
+#' @param show Show the label in plots : default = F
+#' @param object Attribute an arrow to an object type to match colours : "Offense" (default), "Defense", "Coach", "Cone", "Disc" are recognised but can be anything.
+#' @param type What the arrow represents : "Cut" (default), "Throw", "Label" are recognised but can be anything
+#' @param x x-coord for start of arrow (required)
+#' @param y y-coord for start of arrow (required)
+#' @param xend x-coord for end of arrow (required)
+#' @param yend y-coord for end of arrow (required)
+#' @param frame Vector of frames to show the arrow in : default = 1
+#' @param alpha Colour opacity of the arrow : default = 1
+#' @param arrow_shape Shape of the arrow : "straight" (default), "bhrc", "fhrc", "bhio", "fhio" (give curved throwing shapes)
+#' @param label_pos Position of label relative to start or end of arrow : "start_down" (default), "start_up", "end_down", "end_up"
+#' @return A data.frame
+#' @examples
+#' pitch_arrow("T1", type = "Throw", x = 5, y = 37, xend = 5, yend = 90, frame = 3)
+#' pitch_arrow("C1", x = 5, y = 37, xend = 5, yend = 90, frame = 3)
 pitch_arrow <- function(label = "", show = F, object = "Offense", type = "Cut", x, y, xend, yend, frame = 1, alpha = 1, arrow_shape = "straight", label_pos = "start_down"){
   lshow = ""
   if(show == T){
@@ -89,8 +162,17 @@ pitch_arrow <- function(label = "", show = F, object = "Offense", type = "Cut", 
   data.frame(label = label, show = lshow, object = object, type = type, x = x, y = y, xend = xend, yend = yend, frame = frame, alpha = alpha, arrow_shape = arrow_shape, label_pos = label_pos)
 }
 
-player_paths <- function(player_list, objects = c("Offense")){
-  arrows <- player_list |> map(~if(!is.null(.x)){
+#' Convenience function to create pitch_arrows which trace an objects movement path on the pitch e.g. a players cut. You can choose
+#' to only trace specific object types e.g offense players only.
+#'
+#' @param object_list A list of object data.frames e.g. generated by pitch_object (required)
+#' @param objects A vector of the object types to trace : default = "Offense"
+#' @return A data.frame
+#' @examples
+#' object_paths(player_list)
+#' object_paths(player_list, c("Offense","Defense"))
+object_paths <- function(object_list, objects = c("Offense")){
+  arrows <- object_list |> map(~if(!is.null(.x)){
                       .x |>
                        mutate(xend = x, yend = y,
                               x = lag(x), y = lag(y),
@@ -104,7 +186,25 @@ player_paths <- function(player_list, objects = c("Offense")){
   bind_rows(arrows) |> filter(object %in% objects)
 }
 
-throw <- function(player_list,label,show=F,from,to,frame=1,throw_frame,alpha=1,arrow_shape="straight",space_x=NA,space_y=NA,label_pos="start_down"){
+#' Convenience function to create a pitch_arrow dataframe representing a throw between two players in an object list.
+#'
+#' @param object_list List of pitch_objects (required)
+#' @param label Name for the arrow (required)
+#' @param show Show the label in plots : default = F
+#' @param from Label of the throwing object
+#' @param to Label of the receiving object or "space" if into empty space (provide space_x and space_y)
+#' @param frame Vector of frames to show the arrow in : default = 1
+#' @param throw_frame The frame representing the throw and receive positions of the to and from objects
+#' @param alpha Colour opacity of the arrow : default = 1
+#' @param arrow_shape Shape of the arrow : "straight" (default), "bhrc", "fhrc", "bhio", "fhio" (give curved throwing shapes)
+#' @param space_x x-coord of a throw into space (must specify to = "space")
+#' @param space_y y-coord of a throw into space (must specify to = "space")
+#' @param label_pos Position of label relative to start or end of arrow : "start_down" (default), "start_up", "end_down", "end_up"
+#' @return A data.frame
+#' @examples
+#' throw(player_list,"T1", from = "H1", to = "C1", frame = 1:3, throw_frame = 3)
+#' throw(player_list,"T1", from = "H1", to = "space", space_x = 5, space_y = 90, arrow_shape = "bhrc", frame = 2:3, throw_frame = 3)
+throw <- function(object_list,label,show=F,from,to,frame=1,throw_frame,alpha=1,arrow_shape="straight",space_x=NA,space_y=NA,label_pos="start_down"){
   lshow = ""
   if(show == T){
     lshow = label
@@ -139,7 +239,21 @@ throw <- function(player_list,label,show=F,from,to,frame=1,throw_frame,alpha=1,a
   data.frame(label = label, show = lshow, object = object, type = "Throw", x = x, y = y, xend = xend, yend = yend, frame = frame, alpha = alpha, arrow_shape = arrow_shape, label_pos = label_pos)
 }
 
-disc <- function(throw,label,show=F,xjust=c(-1,-1),yjust=c(1,1),alpha=1,frame,release_frame,label_pos="down"){
+#' Convenience function to create a pitch_object which follows the path of an arrow. E.g. a disc following a throw.
+#'
+#' @param throw A pitch_arrow dataframe (required)
+#' @param label Name for the object (required)
+#' @param show  Show the label in plots : default = F
+#' @param xjust 2 value vector showing the x-coord offset for the thrower and receiver : default = c(-1,-1)
+#' @param yjust 2 value vector showing the y-coord offset for the thrower and receiver : default = c(1,1)
+#' @param frame Vector of frames to show the object in : default = 1
+#' @param release_frame The frame where the disc should be released from the thrower : default = 2
+#' @param alpha Colour opacity of the object : default = 1
+#' @param label_pos Position of label relative to start or end of object : "down" (default), "up", "left", "right"
+#' @return A data.frame
+#' @examples
+#' disc(throw_list[[1]],"D1",frame = 1:3, release_frame = 2)
+disc <- function(throw,label,show=F,xjust=c(-1,-1),yjust=c(1,1),frame=1,release_frame=2,alpha=1,label_pos="down"){
     lshow = ""
     if(!show == F){
       lshow = "label"
@@ -160,22 +274,39 @@ disc <- function(throw,label,show=F,xjust=c(-1,-1),yjust=c(1,1),alpha=1,frame,re
     data.frame(label = label,show = lshow,object="Disc",x = x,y = y,frame = frame,alpha = alpha,label_pos = label_pos)
 }
 
+#' Helper function used by plot_play() function. Takes a dataframe of objects/arrows and fills so that each object/arrow appears in all frames
+#'
+#' @param df Dataframe of objects/arrows
+#' @param frames Total number of frames to fill
+#' @return A data.frame
+#' @examples
+#' all_frames(throws,3)
 all_frames <- function(df,frames){
   df |> group_by(label) |>
     complete(frame = full_seq(1:frames,1)) |>
     fill(-frame,.direction="updown")
 }
 
+# Hardcoded plotting variables
 arrowtypes = c("Cut" = "solid", "Throw" = "dashed", "Label" = "dotted")
 obj_cols =c("Disc" = "grey","Offense" = "#018571", "Defense" = "#f8766d", "Coach" = "#0571b0", "Cone" = "darkorange")
 obj_shapes = c("Player"=19,"Disc"=19,"Cone"=17)
 obj_sizes = c("Player"=8,"Disc"=7,"Cone"=4)
 
-plot_play <- function(pitch,arrow_list=NULL,object_list=NULL,static_frame=1,animate=F,keep_arrows=F){
+#' Plotting function to create final graphic with a pitch, objects and arrows.
+#'
+#' @param pitch A pitch ggplot object create by ggpitch() : default = ggpitch()
+#' @param arrow_list List of pitch_arrow dataframes
+#' @param object_list List of pitch_object dataframes
+#' @param static_frame Which frame to show in a static plot : default = 1
+#' @param animate Show all frames so plot can be animated : F (default), T
+#' @param keep_arrows Show arrows in all frames and only apply frames to objects : F (default), T
+#' @return A ggplot object
+#' @examples
+#' plot_play(pitch,arrow_list,object_list,static_frame=2)
+plot_play <- function(pitch=ggpitch(),arrow_list=NULL,object_list=NULL,static_frame=1,animate=F,keep_arrows=F){
   arrows = bind_rows(arrow_list)
   objects = bind_rows(object_list)
-
-  frames = max(arrows$frame,objects$frame)
 
   p = pitch
   if(nrow(arrows) > 0){
@@ -183,7 +314,8 @@ plot_play <- function(pitch,arrow_list=NULL,object_list=NULL,static_frame=1,anim
       arrows = arrows |> filter(frame==static_frame)
     }
     if(animate==T & keep_arrows==T){
-      arrows = arrows |> all_frames(arrows,frames)
+      frames = max(arrows$frame,objects$frame)
+      arrows = all_frames(arrows,frames)
     }
     if(arrows |> filter(arrow_shape %in% c("fhrc","bhio")) |> nrow() > 0){
       p = p + geom_curve(data=arrows |> filter(arrow_shape %in% c("fhrc","bhio")),
